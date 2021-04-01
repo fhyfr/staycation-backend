1a3b468b9d9b6bc845676940caadbd7edd58bed3
Firman <fhaya.firman@gmail.com>
2021-04-01 11:54:19 +0700

    build CRUD features on Bank

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const adminController = require('../controllers/adminController')
+const adminController = require('../controllers/adminController');
+const {upload} = require('../middlewares/multer');
 
 router.get('/dashboard', adminController.viewDashboard);
 
@@ -11,6 +12,11 @@ router.delete('/category/:id', adminController.deleteCategory);
 
 // Endpoint Bank
 router.get('/bank', adminController.viewBank);
+router.post('/bank', upload, adminController.addBank);
+router.put('/bank', upload, adminController.editBank);
+router.delete('/bank/:id', adminController.deleteBank);
+
+// Endpoint Item
 router.get('/item', adminController.viewItem);
 router.get('/booking', adminController.viewBooking);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const adminController = require('../controllers/adminController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', adminController.viewDashboard);
 
 module.exports = router;
