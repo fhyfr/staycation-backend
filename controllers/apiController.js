6aac6337d8b69f0f15974877a36474107ffb5023
Firman <fhaya.firman@gmail.com>
2021-07-26 14:30:01 +0700

    create endpoint for booking

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,6 +3,8 @@ const Treasure = require('../models/Activity');
 const Traveler = require('../models/Booking');
 const Category = require('../models/Category');
 const Bank = require('../models/Bank');
+const Member = require('../models/Member');
+const Booking = require('../models/Booking');
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -97,5 +99,91 @@ module.exports = {
       res.status(500).json({ message: 'Internal server error' });
     }
   },
-  
+
+  bookingPage: async(req, res) => {
+    try {
+      const {
+        itemId,
+        duration,
+        // price,
+        bookingStartDate,
+        bookingEndDate,
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        accountHolder,
+        bankFrom,
+      } = req.body;
+
+      if (!req.file) {
+        return res.status(404).json({ message: 'Image not found' })
+      }
+
+      if(
+        itemId == undefined ||
+        duration == undefined ||
+        // price == undefined ||
+        bookingStartDate == undefined ||
+        bookingEndDate == undefined ||
+        firstName == undefined ||
+        lastName == undefined ||
+        email == undefined ||
+        phoneNumber == undefined ||
+        accountHolder == undefined ||
+        bankFrom == undefined
+      ) {
+        res.status(400).json({ message: 'Please complete all field required' });
+      }
+
+      const item = await Item.findOne({ _id: itemId });
+
+      if (!item) {
+        return res.status(404).json({ message: 'Item not found' });
+      }
+      item.sumBooking++;
+      await item.save();
+
+      let total = item.price * duration;
+      let tax = total * 0.10;
+
+      const invoice = Math.floor(1000000 + Math.random() * 9000000);
+
+      const member = await Member.create({
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+      });
+
+      const newBooking = {
+        invoice,
+        bookingStartDate,
+        bookingEndDate,
+        total: total += tax,
+        itemId: {
+          _id: item.id,
+          title: item.title,
+          price: item.price,
+          duration: duration
+        },
+        memberId: member.id,
+        payments: {
+          proofPayment: `images/${req.file.filename}`,
+          bankFrom: bankFrom,
+          accountHolder: accountHolder,
+        },
+      }
+
+      const booking = await Booking.create(newBooking);
+
+      res.status(201).json({ 
+        message: 'Success booking',
+        data: booking, 
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
 }
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const apiController = require('../controllers/apiController');
+const { upload } = require("../middlewares/multer");
 // const auth = require("../middlewares/auth");
 
 // landing page
 router.get('/landing-page', apiController.landingPage);
 router.get('/detail-page/:id', apiController.detailPage);
+router.post('/booking-page', upload, apiController.bookingPage);
 
 
 
